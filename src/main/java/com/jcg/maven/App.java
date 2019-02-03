7d767f113a9f62163e50b1d828fad76706a3adea
karthik <nkarthik.gmail.com>
2019-02-03 19:50:03 +0530

    added

--- src/main/java/com/jcg/maven/App.java
+++ src/main/java/com/jcg/maven/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Karthik has updated in dev1 branch!" );
+        System.out.println( "File updated in Master branch!" );
     }
 }
